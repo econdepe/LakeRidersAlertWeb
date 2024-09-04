f5d7860303594789c917fce02f89dd9b7f262d69
econdepe <econdepe@protonmail.com>
2024-09-04 15:18:51 +0200

    Create DB models

--- lakeridersalert_web/app/models.py
+++ lakeridersalert_web/app/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Calendar(models.Model):
+    datetime = models.DateTimeField("session date")
+    members = models.CharField("session members", max_length=200)
+
+
+class Alert(models.Model):
+    calendar_entry = models.ForeignKey(Calendar, on_delete=models.PROTECT)
+    timestamp = models.PositiveIntegerField("alert timestamp")
+    count = models.PositiveSmallIntegerField("number of free slots")
+    members = models.CharField("members dropped", max_length=200)
