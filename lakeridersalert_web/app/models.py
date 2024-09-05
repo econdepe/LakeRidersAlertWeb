d47d060a57d69caf1a867c8f6ab582c9ae605eb9
econdepe <econdepe@protonmail.com>
2024-09-05 18:21:37 +0200

    Modify type of db entry

--- lakeridersalert_web/app/models.py
+++ lakeridersalert_web/app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Session(models.Model):
-    datetime = models.DateTimeField("session date")
+    datetime = models.CharField("session date", max_length=13)
     members = models.CharField("session members", max_length=200)
 
 
 class Alert(models.Model):
     calendar_entry = models.ForeignKey(Session, on_delete=models.PROTECT)
-    timestamp = models.PositiveIntegerField("alert timestamp")
+    timestamp = models.PositiveIntegerField("alert UNIX timestamp")
     count = models.PositiveSmallIntegerField("number of free slots")
     members = models.CharField("members dropped", max_length=200)
